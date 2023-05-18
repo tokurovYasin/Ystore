94cd0f586b6965f0acf38016b9b3c9d14d399c37
Yasin <yasin4911@gmail.com>
2023-05-18 19:12:00 +0600

    ss

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,12 +1,202 @@
 import React from "react";
-
+import "./AboutUs.css";
+import { Margin } from "@mui/icons-material";
 const AboutUs = () => {
   return (
-    <div>
-      <img
-        src="https://www.istore.kg/media/mainpageslider/customized-macs-m2-pc.webp"
-        alt=""
-      />
+    <div className="AboutUs">
+      <div className="About_us">
+        <img src="https://www.istore.kg/static/img/home.svg" alt="" />
+        <img src="https://www.istore.kg/static/img/left-bread.svg" alt="" />
+        <p className="AU"> О нас</p>
+      </div>
+      <div className="AboutUs_header">
+        <h1 className="AboutUs_header_text">
+          iStore - магазин техники Apple <br /> в Бишкеке
+        </h1>
+        <img
+          className="AboutUs_header_img"
+          src="https://www.istore.kg/media/about_us_page/2020.20.26-00015_r9b2z4I.jpg"
+          alt=""
+        />
+      </div>
+      <div className="AboutUs_info">
+        <div className="AboutUs_info_block1">
+          <h2 className="AboutUs_info_block1_h2">
+            У нас можно приобрести всю линейку продукций компании Apple по самым
+            привлекательным ценам
+          </h2>
+          <p className="AboutUs_info_block1_text">
+            С 2013 года мы знакомим бишкекчан с полной линейкой продукции Apple,
+            а также предлагаем большой выбор дополнительных устройств, гаджетов
+            и аксессуаров. В магазине вы можете протестировать iMac, MacBook,
+            iPad или iPhone, все девайсы в открытом доступе.
+            <br />
+            <br />
+            Наши опытные консультанты ответят на все вопросы и помогут
+            определиться с выбором, а главное — подберут то, что подойдет именно
+            вам. Кроме того, мы предлагаем нашим клиентам полное гарантийное
+            обслуживание, неограниченное время сервисного обслуживания по
+            программной части, бонусы по накопительной программе, выгодные
+            предложения и акции! <br />
+            Реквизиты: ИП Арса К.Р.
+            <br />
+            <br />
+            Всегда рады видеть вас!
+          </p>
+        </div>
+        <div className="AboutUs_info_block2">
+          <div className="AboutUs_info_block2_1">
+            <div className="AboutUs_info_block2_1_1">
+              <img
+                className="AboutUs_info_block2_1_1_img"
+                src="https://www.istore.kg/media/about_us_page/headphones_support_1.png"
+                alt=""
+              />
+              <h6 className="AboutUs_info_block2_1_1_h6">
+                Консультация и сервис
+              </h6>
+            </div>
+            <div className="AboutUs_info_block2_1_2">
+              <p className="AboutUs_info_block2_1_2_text">
+                Профессиональный и приветливый персонал; подбор и предложение
+                только оптимальных решений; персональный менеджер
+              </p>
+            </div>
+          </div>
+          <div className="AboutUs_info_block2_1">
+            <div className="AboutUs_info_block2_1_1">
+              <img
+                className="AboutUs_info_block2_1_1_img"
+                src="https://www.istore.kg/media/about_us_page/Layer_1.png"
+                alt=""
+              />
+              <h6 className="AboutUs_info_block2_1_1_h6">Цены и расчеты</h6>
+            </div>
+            <div className="AboutUs_info_block2_1_2">
+              <p className="AboutUs_info_block2_1_2_text">
+                Любая форма оплаты: наличный и безналичный расчет, банковские
+                карты Visa, MasterCard; возможность приобрести продукцию в
+                кредит
+              </p>
+            </div>
+          </div>
+          <div className="AboutUs_info_block2_1">
+            <div className="AboutUs_info_block2_1_1">
+              <img
+                className="AboutUs_info_block2_1_1_img"
+                src="https://www.istore.kg/media/about_us_page/Wrench_1.png"
+                alt=""
+              />
+              <h6 className="AboutUs_info_block2_1_1_h6">
+                Постпродажный сервис
+              </h6>
+            </div>
+            <div className="AboutUs_info_block2_1_2">
+              <p className="AboutUs_info_block2_1_2_text">
+                Консультации по использованию; гарантийное обслуживание
+              </p>
+            </div>
+          </div>
+          <div className="AboutUs_info_block2_1">
+            <div className="AboutUs_info_block2_1_1">
+              <img
+                className="AboutUs_info_block2_1_1_img"
+                src="https://www.istore.kg/media/about_us_page/Box_4.png"
+                alt=""
+              />
+              <h6 className="AboutUs_info_block2_1_1_h6">Всегда в наличии</h6>
+            </div>
+            <div className="AboutUs_info_block2_1_2">
+              <p className="AboutUs_info_block2_1_2_text">
+                Айфоны, Айпады, Макбуки, а также оригинальные аксессуары для
+                Ваших гаджетов
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="AboutUs_feedback">
+        <h2 className="AboutUs_feedback_h2">Обратная связь</h2>
+        <div className="AboutUs_feedback_block">
+          <div className="AboutUs_feedback_block_1">
+            <iframe
+              className="footer_map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.1304438103593!2d4.367699815745808!3d50.8472686795321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c482a7578471%3A0xa7713c3e13f82b7d!2sSouth%20Asia%20Democratic%20Forum!5e0!3m2!1sru!2skg!4v1679565990272!5m2!1sru!2skg"
+              width="550"
+              height="350"
+              style={{ border: "0.5 solid #000" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="AboutUs_feedback_block_2">
+            <div className="AboutUs_feedback_block2_1">
+              <div className="AboutUs_feedback_block2_1_1">
+                <img
+                  className="AboutUs_feedback_block2_1_1_img"
+                  src="https://www.istore.kg/static/img/phone.svg"
+                  alt=""
+                />
+                <h6 className="AboutUs_feedback_block2_1_1_h6">Наши номера</h6>
+              </div>
+              <div className="AboutUs_feedback_block2_1_2">
+                <p className="AboutUs_feedback_block2_1_2_text">
+                  +996 (555) 802 000 <br />
+                  +996 (505) 802 000
+                </p>
+              </div>
+            </div>
+            <div className="AboutUs_feedback_block2_1">
+              <div className="AboutUs_feedback_block2_1_1">
+                <img
+                  className="AboutUs_feedback_block2_1_1_img"
+                  src="https://www.istore.kg/static/img/time-five.svg"
+                  alt=""
+                />
+                <h6 className="AboutUs_feedback_block2_1_1_h6">
+                  График работы
+                </h6>
+              </div>
+              <div className="AboutUs_feedback_block2_1_2">
+                <p className="AboutUs_feedback_block2_1_2_text">
+                  Пн - Вс 10:00 - 20:00
+                </p>
+              </div>
+            </div>
+            <div className="AboutUs_feedback_block2_1">
+              <div className="AboutUs_feedback_block2_1_1">
+                <img
+                  className="AboutUs_feedback_block2_1_1_img"
+                  src="https://www.istore.kg/static/img/mail.svg"
+                  alt=""
+                />
+                <h6 className="AboutUs_feedback_block2_1_1_h6">Наша почта</h6>
+              </div>
+              <div className="AboutUs_feedback_block2_1_2">
+                <p className="AboutUs_feedback_block2_1_2_text">
+                  office@istore.kg
+                </p>
+              </div>
+            </div>
+            <div className="AboutUs_feedback_block2_1">
+              <div className="AboutUs_feedback_block2_1_1">
+                <img
+                  className="AboutUs_feedback_block2_1_1_img"
+                  src="https://www.istore.kg/static/img/pin.svg"
+                  alt=""
+                />
+                <h6 className="AboutUs_feedback_block2_1_1_h6">Адрес</h6>
+              </div>
+              <div className="AboutUs_feedback_block2_1_2">
+                <p className="AboutUs_feedback_block2_1_2_text">
+                  пр. Манаса, 40 (пер. ул. Киевская)
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
